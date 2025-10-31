bd3a5334158137d0b2cb51fe43779af495caf210
clover9593 <clover9593@gmail.com>
2025-10-31 23:56:10 +0900

    feat: 출력 템플릿 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -79,11 +79,11 @@ public class Application {
             }
 
         }
-        System.out.println("3개 일치 (5,000원) - " + matching[0]);
-        System.out.println("4개 일치 (50,000원) - " + matching[1]);
-        System.out.println("5개 일치 (1,500,000원) - " + matching[2]);
-        System.out.println("5개 일치, 보너스  볼 일치 (30,000,000원) - " + matching[3]);
-        System.out.println("6개 일치 (2,000,000,000원) - " + matching[4]);
+        System.out.println("3개 일치 (5,000원) - " + matching[0] + "개");
+        System.out.println("4개 일치 (50,000원) - " + matching[1] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + matching[2] + "개");
+        System.out.println("5개 일치, 보너스  볼 일치 (30,000,000원) - " + matching[3] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + matching[4] + "개");
 
         double profitRate = (double) totalMoney / lottoPrice * 100.0;
         System.out.println("총 수익률은 " + String.format("%.1f", profitRate) + "%입니다.");
